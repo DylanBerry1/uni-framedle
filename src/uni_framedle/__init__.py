b22ee7d491827131945318e18df346206d4fae12
Dylan Berry <dylanjberry@icloud.com>
2026-06-03 23:38:13 -0700

    finished functionality

--- src/uni_framedle/__init__.py
+++ src/uni_framedle/__init__.py
@@ -17,7 +17,6 @@ def generate_move() -> pd.DataFrame():
     return get_index(r)
 
 def handle_adv_frames(adv) -> str:
-    print(adv)
     if (isinstance(adv, float) and math.isnan(adv)) or (adv == '-') or (adv == None):
         return "NA"
     if adv.lower() == "varies":
@@ -39,16 +38,17 @@ def handle_startup(su) -> int:
 
 def high_low(num, target):
     if num == target:
-        return "Spot on!"
+        return "spot on!"
     elif num > target:
-        return "Too high!"
+        return "too high!"
     else:
-        return "Too low!"
+        return "too low!"
     
 
 def main() -> None:
     move_choice = generate_move()
     
+    count = 0
     
     print("Hello! Today, you shall be playing a wordle-like for the frame data of character's moves from Under Night In Birth 2: [sys-celes].")
     print("\n For the rules, you will guess the startup and advantage of moves. For every number you guess, it will tell you higher or lower until you have gotten all of them on the money.")
@@ -57,7 +57,7 @@ def main() -> None:
     guess_startup = -1
     guess_adv_min, guess_adv_max = -999, -999
     
-    
+    target_startup = handle_startup(move_choice.startup)
     processed_adv = handle_adv_frames(move_choice.frameAdv)
     target_guess_min, target_guess_max = -999, -999
     
@@ -66,26 +66,40 @@ def main() -> None:
     else:
         target_guess_min = handle_adv_frames(move_choice.frameAdv)
     
-    while guess_startup != move_choice.startup and guess_adv_min != target_guess_min and guess_adv_max != target_guess_max:
+    while True:
+        count += 1
         print(f"Move to guess: {move_choice.chara}, {move_choice.input}")
         
         print("Guess for startup:")
         inp = input()
         guess_startup = re.search('\\d+', inp).group(0)
+        
         guess_string = ""
+        result_string = ""
+        
         if target_guess_max != -999:
-            Print("\nGuess for minimum advantage:")
+            print("\nGuess for minimum advantage:")
             inp = input()
             guess_adv_min = re.search('-\\d+|\\d+', inp).group(0)
             
-            Print("\nGuess for maximum advantage:")
+            print("\nGuess for maximum advantage:")
             inp = input()
             guess_adv_max = re.search('-\\d+|\\d+', inp).group(0)
             guess_string = f"{guess_adv_min} to {guess_adv_max}"
+            result_string = f"The minimum advantage is {high_low(int(guess_adv_min), target_guess_min)} The max advantage is {high_low(int(guess_adv_max), target_guess_max)}"
         else:
-            Print("\nGuess for advantage:")
+            print("\nGuess for advantage:")
             inp = input()
             guess_adv_min = re.search('-\\d+|\\d+', inp).group(0)
             guess_string = f"{guess_adv_min}"
+            result_string = f"The advantage you have guessed is {high_low(int(guess_adv_min), target_guess_min)}"
             
-        print(f"Recap: You guessed that the startup of {move_choice.chara}'s {move_choice.input} is {guess_startup}, and that the advantage is ")
+        print(f"Recap: You guessed that the startup of {move_choice.chara}'s {move_choice.input} is {guess_startup}, and that the advantage is {guess_string}.")
+        
+        print(f"For the startup, your guess is {high_low(int(guess_startup), target_startup)} {result_string}")
+        
+        if high_low(int(guess_startup), target_startup) == 'spot on!' and high_low(int(guess_adv_min), target_guess_min) == 'spot on!' and high_low(int(guess_adv_max), target_guess_max) == 'spot on!':
+            break
+
+    print(f"Congratulations! You got it in {count}!")
+        
